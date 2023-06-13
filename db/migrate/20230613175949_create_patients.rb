2b5f1a996f6fd059bc7ada40f572f278e70e566c
LuoLuann <luo.luann@gmail.com>
2023-06-13 15:12:06 -0300

    criacao de controller e model endereco

---
+++ db/migrate/20230613175949_create_patients.rb
@@ -0,0 +1,12 @@
+class CreatePatients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :patients do |t|
+      t.string :fullname
+      t.date :date_of_birth
+      t.string :cpf
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20230613180331_create_addresses.rb
@@ -0,0 +1,12 @@
+class CreateAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :addresses do |t|
+      t.string :cep
+      t.string :city
+      t.string :street
+      t.string :next
+
+      t.timestamps
+    end
+  end
+end
