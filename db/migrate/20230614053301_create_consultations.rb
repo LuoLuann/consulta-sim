767143045399bc3787d6a2c94684adb1ff0d4844
LuoLuann <luo.luann@gmail.com>
2023-06-14 02:42:52 -0300

    feito model e migration de consulta, e associado a paciente

---
+++ db/migrate/20230614053301_create_consultations.rb
@@ -0,0 +1,10 @@
+class CreateConsultations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :consultations do |t|
+      t.date :date
+      t.time :time
+
+      t.timestamps
+    end
+  end
+end
