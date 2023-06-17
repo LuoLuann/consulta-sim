6df901c1e8fb8e9eaf286169263e537b975fe1cf
LuoLuann <luo.luann@gmail.com>
2023-06-17 19:01:52 -0300

    validações do model paciente feitas

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,13 +4,10 @@ class Patient < ApplicationRecord
 
   has_many :consultations
 
-  validates :fullname, presence: true, length: { minimum: 10, maximum: 70}
-  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
-  #validates :birthdate_validation
+  validates :fullname, presence: true, length: { minimum: 10, maximum: 255}, format: { with: /\A[a-zA-Z]+\z/. message: "Deve conter apenas letras" }
+  validates :email, presence: { message: "O e-mail é obrigatório"}, uniqueness: { message: "Este e-mail já está em uso" }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "O e-mail possui um formato inválido" }
+  validates_date :birthdate, presence: { message: "A data de nascimento é obrigatória" } , on_or_before: lambda { Date.current }, on_or_after: lambda { 120.years.ago }
+  validates :cpf, presence: true, uniqueness: true, lenght: { is: 11 }, numerically: ( only_integer: true, message: "O CPF deve conter apenas números" )
 
-  # def birthdate_validation
-  #   return birthdate.present?
-
-  # if birthdate_validation
 
 end
