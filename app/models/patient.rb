a905678addfe7119fcf2ac1dd66403a4c09f5663
LuoLuann <luo.luann@gmail.com>
2023-06-17 19:18:18 -0300

    fix model paciente: trocar um ponto por uma virgula em uma validação

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,7 +4,7 @@ class Patient < ApplicationRecord
 
   has_many :consultations
 
-  validates :fullname, presence: true, length: { minimum: 10, maximum: 255}, format: { with: /\A[a-zA-Z]+\z/. message: "Deve conter apenas letras" }
+  validates :fullname, presence: true, length: { minimum: 10, maximum: 255}, format: { with: /\A[a-zA-Z]+\z/, message: "Deve conter apenas letras" }
   validates :email, presence: { message: "O e-mail é obrigatório"}, uniqueness: { message: "Este e-mail já está em uso" }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "O e-mail possui um formato inválido" }
   validates_date :birthdate, presence: { message: "A data de nascimento é obrigatória" } , on_or_before: lambda { Date.current }, on_or_after: lambda { 120.years.ago }
   validates :cpf, presence: true, uniqueness: true, lenght: { is: 11 }, numerically: ( only_integer: true, message: "O CPF deve conter apenas números" )
