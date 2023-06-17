abc6b8e769b7f554e2fd681d984529043f11d958
LuoLuann <luo.luann@gmail.com>
2023-06-17 20:26:51 -0300

    consertando erros de sintaxe

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,10 +1,10 @@
 class Doctor < ApplicationRecord
   has_many :consultations
 
-  validates :fullname, presence: true, length: { minimum: 10, maximum: 255 }, format: { with: /\A[a-zA-Z]+\z/, message: "Deve conter apenas letras" }
-  validates :email, presence: { message: "O e-mail é obrigatório"}, uniqueness: { message: "Este e-mail já está em uso" }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "O e-mail possui um formato inválido" }
-  validates :cpf, presence: { message: "O CPF é obrigatória" }, uniqueness: { message: "Este CPF já está em uso" }, lenght: { is: 11 }, numerically: ( only_integer: true, message: "O CPF deve conter apenas números" )
-  validates :specialty, presence: true, length: { minimum: 7, maximum: 255 }, format: { with: /\A[a-zA-Z]+\z/. }
-  validate :crm , presence: { message: "O CRM é obrigatório" }, uniqueness: { message: "Este CRM já está em uso" }, lenght: { is: 6 }, numerically: { only_integer: true }
+  validates :fullname, presence: true, length: { minimum: 10, maximum: 255 }, format: { with: /\A[a-zA-Z]+\z/, message: " deve conter apenas letras" }
+  validates :email, presence: { message: "O e-mail é obrigatório"}, uniqueness: { message: " já está em uso" }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: " possui um formato inválido" }
+  validates :cpf, presence: { message: " é obrigatório" }, uniqueness: { message: " já está em uso" }, length: { is: 11, message: " deve conter 11 dígitos" }, numericality: { only_integer: true, message: " deve conter apenas números" }
+  validates :specialty, presence: true, length: { minimum: 7, maximum: 255 }, format: { with: /\A[a-zA-Z\s]+\z/ }
+  validates :crm , presence: { message: " é obrigatório" }, uniqueness: { message: " já está em uso" }, length: { is: 6 }, numericality: { only_integer: true }
 
 end
