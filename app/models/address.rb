f236dfd0e606ef13b89373da1e596c2af154a3dc
LuoLuann <luo.luann@gmail.com>
2023-06-17 20:58:54 -0300

    validações para endereço finalizadas

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,10 @@
 class Address < ApplicationRecord
+  belongs_to :patient
+
+  validates :cep, presence: true, length: { is: 8, message: " deve conter 8 dígitos" }, numericality: { only_integer: true, message: " deve conter apenas números" }
+  validates :city, presence: true, length: { minimum: 3, maximum: 255}
+  validates :neighborhood, presence: true, length: { minimum: 3, maximum: 255}
+  validates :street, presence: true, length: { minimum: 3, maximum: 255}
+  validates :next, presence: true, length: { minimum: 3, maximum: 255}
+
 end
