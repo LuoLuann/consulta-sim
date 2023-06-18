1d9797b48edba18d5b3d626d2f0159bfbf0f29a2
LuoLuann <luo.luann@gmail.com>
2023-06-18 00:19:56 -0300

    alterando algumas regras da validação

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,10 +1,10 @@
 class Doctor < ApplicationRecord
   has_many :consultations
 
-  validates :fullname, presence: true, length: { minimum: 10, maximum: 255 }, format: { with: /\A[a-zA-Z]+\z/, message: " deve conter apenas letras" }
+  validates :fullname, presence: true, length: { minimum: 10, maximum: 255}, format: { with: /\A[a-zA-Z\s]+\z/, message: "Deve conter apenas letras" }
   validates :email, presence: { message: "O e-mail é obrigatório"}, uniqueness: { message: " já está em uso" }, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: " possui um formato inválido" }
   validates :cpf, presence: { message: " é obrigatório" }, uniqueness: { message: " já está em uso" }, length: { is: 11, message: " deve conter 11 dígitos" }, numericality: { only_integer: true, message: " deve conter apenas números" }
-  validates :specialty, presence: true, length: { minimum: 7, maximum: 255 }, format: { with: /\A[a-zA-Z\s]+\z/ }
+  validates :specialty, presence: true, length: { minimum: 3, maximum: 255 }, format: { with: /\A[a-zA-Z\s]+\z/ }
   validates :crm , presence: { message: " é obrigatório" }, uniqueness: { message: " já está em uso" }, length: { is: 6 }, numericality: { only_integer: true }
 
 end
