aa1f6895216f9d3c197c37a46e21a78533af0790
LuoLuann <luo.luann@gmail.com>
2023-06-17 21:30:36 -0300

    validações para consulta finalizadas

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -1,4 +1,17 @@
 class Consultation < ApplicationRecord
   belongs_to :patient
   belongs_to :doctor
+
+  validates_date :date, presence: { message: " é obrigatório"}
+  validates :time, presence: { message: " é obrigatório"}
+  validates :patient_id, presence: { message: " é obrigatório"}
+  validates :doctor_id, presence: { message: " é obrigatório"}
+
+  validate :date_within_range
+
+  def date_within_range
+    if date.present? && (date < Date.current || date > Date.current + 4.months)
+      errors.add(:date, ": Consulta deve estar dentro do limite de 4 meses após a data atual")
+    end
+  end
 end
