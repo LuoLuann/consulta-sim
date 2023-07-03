c23c4846b04ad0e21b1f373b954056e50bd776bc
LuoLuann <luo.luann@gmail.com>
2023-07-03 14:23:36 -0300

    testes para o model doctor

--- test/models/doctor_test.rb
+++ test/models/doctor_test.rb
@@ -4,4 +4,24 @@ class DoctorTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save doctor with invalid crm" do
+    doctor = Doctor.new fullname: "Luann Ferreira",
+                        cpf: "123456789101",
+                        email: "luann@gmail.com",
+                        specialty: "Médico geral",
+                        crm: "abc"
+
+    assert_not doctor.save
+  end
+
+  test "should not save doctor without specialty" do
+    doctor = Doctor.new fullname: "Luann Ferreira",
+                        cpf: "123456789101",
+                        email: "luann@gmail.com",
+                        specialty: "",
+                        crm: "abc"
+
+    assert_not doctor.save
+  end
 end
