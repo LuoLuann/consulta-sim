ed43f2003f59f3cb1b48552430fdf5a6eb555161
LuoLuann <luo.luann@gmail.com>
2023-07-04 12:05:11 -0300

    teste model consultation

--- test/models/consultation_test.rb
+++ test/models/consultation_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class ConsultationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "save consultation" do
+    consultation = Consultation.new(patient: patients(:one),
+                  doctor: doctors(:one),
+                  time: Time.now + 1.hour,
+                  date: Date.current
+                )
+
+    assert consultation.save
+  end
+
+  test "should not save without doctor" do
+    consultation = Consultation.new(patient: patients(:one),
+                                    time: Time.now + 1.hour,
+                                    date: Date.current
+                                  )
+
+    assert_not consultation.save
+  end
 end
